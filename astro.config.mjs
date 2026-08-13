177212a88a24945b4b2ac915fc4205f63c3ebc3c
ctharaka <tharakachalina@gmail.com>
2026-08-13 17:40:13 +0530

    Add site URL to astro config for sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://freefiletools.com',
+  site: 'https://freefiletools.pages.dev',
   output: 'static',
   integrations: [sitemap()],
   devToolbar: {
